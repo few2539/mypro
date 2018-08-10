73968975b1b61c5c8ded4e5c43223562e7422479
fewphuridate <few_phuridate@hotmail.com>
2018-08-11 01:06:44 +0700

    slick bug a lot

--- dist/assets/styles/js/main.js
+++ dist/assets/styles/js/main.js
@@ -3132,3 +3132,32 @@ var selector = '.js-animation';
 
 // });
 console.log('eeeww');
+
+window.SlickCarousel = {
+
+    onReady: function () {
+        SlickCarousel.initation();
+    },
+
+    initation: function () {
+      
+        $(document).ready(function () {
+            
+        
+            $('.myslick').slick({
+                infinite: true,
+                // slidesToShow: 3,
+                // slidesToScroll: 3,
+                dots:true,
+                arrows:true,
+                autoplay:true,
+                autoplaySpeed:2000,
+
+            });
+        });
+
+    }
+
+
+}
+$(document).ready(SlickCarousel.onReady);
--- src/assets/styles/js/slick-c.js
+++ src/assets/styles/js/slick-c.js
@@ -1,1 +1,30 @@
 console.log('eeeww');
+
+window.SlickCarousel = {
+
+    onReady: function () {
+        SlickCarousel.initation();
+    },
+
+    initation: function () {
+      
+        $(document).ready(function () {
+            
+        
+            $('.myslick').slick({
+                infinite: true,
+                // slidesToShow: 3,
+                // slidesToScroll: 3,
+                dots:true,
+                arrows:true,
+                autoplay:true,
+                autoplaySpeed:2000,
+
+            });
+        });
+
+    }
+
+
+}
+$(document).ready(SlickCarousel.onReady);
